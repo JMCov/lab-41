58e779220a9705981aac5446c8b8f6c7ae1ec628
Jordan Covington <Atlantafan1087@gmail.com>
2023-04-10 17:21:07 -0700

    Lab 41 Complete

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as SMS from 'expo-sms';
 
 export default function App() {
+  const [contact, setContact] = useState(null);
+
+  const getRandomContact = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      const filteredContacts = data.filter(
+        (contact) => contact.phoneNumbers && contact.phoneNumbers.length > 0
+      );
+
+      const randomIndex = Math.floor(Math.random() * filteredContacts.length);
+      const selectedContact = filteredContacts[randomIndex];
+
+      setContact(selectedContact);
+    }
+  };
+
+  const sendMessage = async () => {
+    if (contact) {
+      const isAvailable = await SMS.isAvailableAsync();
+      if (isAvailable) {
+        const { result } = await SMS.sendSMSAsync(
+          [contact.phoneNumbers[0].number],
+          'Hello from my app!'
+        );
+
+        if (result === 'sent') {
+          alert('Message sent successfully!');
+        } else {
+          alert('Failed to send message');
+        }
+      } else {
+        alert('SMS is not available on this device');
+      }
+    } else {
+      alert('Please select a contact before sending a message');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {contact ? (
+        <View style={styles.contact}>
+          <Text style={styles.contactName}>{contact.name}</Text>
+          <Text style={styles.contactPhone}>
+            {contact.phoneNumbers[0].number}
+          </Text>
+        </View>
+      ) : (
+        <Text style={styles.noContact}>No contact selected</Text>
+      )}
+
+      <Button title="Select random contact" onPress={getRandomContact} />
+
+      <Button title="Send message" onPress={sendMessage} disabled={!contact} />
     </View>
   );
 }
@@ -13,8 +69,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  contact: {
+    marginBottom: 20,
+  },
+  contactName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  contactPhone: {
+    fontSize: 16,
+  },
+  noContact: {
+    fontSize: 18,
+    marginBottom: 20,
   },
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# Random Texting App
+
+## Features
+
+- Click the the Select random contact to send a text to someone in your contact list
+- Why? IDK!
+- Hit the Send Message button to open up your default messaging app. 
+- Default Message is currently Hello From My App!
+
+## Screen Shots
+
+![Screen Shot 1](./assets/SS1.PNG)
+![Screen Shot 1](./assets/SS2.PNG)
